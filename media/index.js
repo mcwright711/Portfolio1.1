5d4b3ccf5c421accd4c6762b6824dd4946e49cdd
mcwright711 <118693689+mcwright711@users.noreply.github.com>
2023-05-06 23:16:12 -0600

    still not affixed

--- media/index.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-    $('design-process').scrollspy({target: '#list-sidebar'});
-});
---
+++ project1.js
@@ -0,0 +1,24 @@
+/*$(document).ready(function(event) {
+  var offsetTop = $('#list-sidebar').offset().top - 141;
+  $(window).scroll(function() {
+    if ($(window).scrollTop() >= offsetTop) {
+      $('#list-sidebar').addClass('affix');
+    } else {
+      $('#list-sidebar').removeClass('affix');
+    }
+  });
+});*/
+/*$(window).on('scroll', function (event) {
+    var scrollValue = $(window).offset().top-141;
+    if (scrollValue > 141) {
+        $('#list-sidebar').addClass('affix');
+    } else{
+        $('#list-sidebar').removeClass('affix');
+    }
+});*/
+/*$('#list-sidebar').affix({
+    offset: {
+        top: $('.navbar').outerHeight() - 141
+        bottom: $('footer').outerHeight() + 50
+    }
+})*/
