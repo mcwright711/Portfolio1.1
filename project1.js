f50bdee243ad64e0b10d16277c4248d5701a3ecd
mcwright711 <118693689+mcwright711@users.noreply.github.com>
2023-05-23 22:16:50 -0600

    typing effect mostly working
    Added Name to monogram
    typing effect mostly working

--- project1.js
+++ project1.js
@@ -1,32 +1,10 @@
-/*$(document).ready(function(event) {
-  var offsetTop = $('#list-sidebar').offset().top - 141;
-  $(window).scroll(function() {
-    if ($(window).scrollTop() >= offsetTop) {
-      $('#list-sidebar').addClass('affix');
-    } else {
-      $('#list-sidebar').removeClass('affix');
-    }
-  });
-});*/
-/*$(window).on('scroll', function (event) {
-    var scrollValue = $(window).offset().top-141;
-    if (scrollValue > 141) {
-        $('#list-sidebar').addClass('affix');
-    } else{
-        $('#list-sidebar').removeClass('affix');
-    }
-});*/
-/*$('#list-sidebar').affix({
-    offset: {
-        top: $('.navbar').outerHeight() - 141
-        bottom: $('footer').outerHeight() + 50
-    }
-})*/
-/*$('.ul').affix({offset: {top: 150} });*/
-/*$(".card" ).hover(
-    function() {
-      $(this).addClass('shadow-lg').css('cursor', 'pointer'); 
-    }, function() {
-      $(this).removeClass('shadow-lg');
-    }
-  ); */ 
+// Adds delay to CSS animation and makes
+// the animation repeat itself
+$("#restart").queue(function() {  
+  var original = $(this);
+  var newClass = original.clone(true);
+  setInterval(function() {
+      original.before(newClass);
+      $("." + original.attr("class") + ":last").remove();
+  },47000);
+});
