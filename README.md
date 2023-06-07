b0db2492547dd7f7f29e40e9930a8c4c7902cc15
mcwright711 <118693689+mcwright711@users.noreply.github.com>
2023-06-06 22:56:26 -0600

    linkedin and monogram
    updated to svg

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Portfolio1.1
-Portfolio using bootstrap
+Portfolio for Marla C. Wright, UX/UI Designer. Coded by the designer, using Bootstrap.  For more information, please visit her linkedin page: https://linkedin.com/in/marla-c-wright.
--- project1.js
+++ project1.js
@@ -1,13 +1,3 @@
-// Adds delay to CSS animation and makes
-// the animation repeat itself
-//$("#restart").queue(function() {  
-  //var original = $(this);
-  //var newClass = original.clone(true);
- // setInterval(function() {
-    //  original.before(newClass);
-   //   $("." + original.attr("class") + ":last").remove();
-  //},48000);
-//});
 var TxtType = function(el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
