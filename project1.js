4ffbf0566f17f4c9191bfd1a497a73e9fc94348a
mcwright711 <118693689+mcwright711@users.noreply.github.com>
2023-10-27 15:04:14 -0600

    updates
    fixed spacing and added gap in about page. fixed hero image and spyscroll bar in aupost page to align. updated email address. added book titles to about page. removed footer padding where i could.

--- project1.js
+++ project1.js
@@ -12,13 +12,78 @@ $(document).ready(function() {
   // Scrollspy effect
  $('section').scrollspy({ target: "#navbar-example2" });
 
-  // Pop-up modal effect
-  //setTimeout(function() {
-  // $("#lego_modal").fadeIn("slow");
-  //}, 800);
 
-//exclude parallax effect from images
-//$('.parallax_row img').addClass('full_width');
-//});
+  document.addEventListener("DOMContentLoaded", function() {
+    var parallaxRowElements = document.querySelectorAll(".parallax_row");
+    var captionElements = document.querySelectorAll(".caption");
+  
+    for (var i = 0; i < parallax_RowElements.length; i++) {
+      var element = parallaxRowElements[i];
+      element.style.removeProperty ("max-width");
+    }
+  
+    for (var i = 0; i < captionElements.length; i++) {
+      var element = captionElements[i];
+      element.style.removeProperty("max-width");
+    }
+  });
+ // Select elements with the class "wobble"
+var wobbleElements = document.querySelectorAll('.wobble');
 
-//Note to self: create media-query for margin or padding when footer is in 1 column, small screen 10/5/23
+// Function to handle the animation
+function animateElement(el) {
+  if (!el.classList.contains('animating') ) {
+    el.classList.add('animating');
+
+    var letters = el.innerText.split('');
+
+    setTimeout(function () {
+      el.classList.remove('animating');
+    }, (letters.length + 1) * 50);
+
+    var animationName = el.dataset.animation;
+    if (!animationName) {
+      animationName = "jump";
+    }
+
+    el.innerText = '';
+
+    letters.forEach(function (letter) {
+      if (letter == " ") {
+        letter = "&nbsp;";
+      }
+      el.innerHTML += '<span class="letter">' + letter + '</span>';
+    });
+
+    var letterElements = el.querySelectorAll('.letter');
+    letterElements.forEach(function (letter, i) {
+      setTimeout(function () {
+        letter.classList.add(animationName);
+      }, 50 * i);
+    });
+  }
+}
+
+// Function to check if an element is in the viewport
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Function to handle the scroll event
+function handleScroll() {
+  wobbleElements.forEach(function (el) {
+    if (isElementInViewport(el)) {
+      animateElement(el);
+    }
+  });
+}
+
+// Add an event listener for the scroll event on the window
+window.addEventListener('scroll', handleScroll);
+  });
