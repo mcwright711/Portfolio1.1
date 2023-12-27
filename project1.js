d317fb21ce540ada93af3ad72e598474fcecba8b
mcwright711 <118693689+mcwright711@users.noreply.github.com>
2023-12-26 22:33:25 -0700

    animations to wolfnest page
    condensing html code to css code. adding animations to titles on wolfnest page. bounce and slide-in effect still not working

--- project1.js
+++ project1.js
@@ -10,20 +10,43 @@ $(document).ready(function() {
   });
 
   // Scrollspy effect
- $('section').scrollspy({ target: "#navbar-example2" });
+  $('section').scrollspy({ target: "#navbar-example2" });
 
-
-  document.addEventListener("DOMContentLoaded", function() {
-    var parallaxRowElements = document.querySelectorAll(".parallax_row");
-    var captionElements = document.querySelectorAll(".caption");
-  
-    for (var i = 0; i < parallax_RowElements.length; i++) {
-      var element = parallaxRowElements[i];
-      element.style.removeProperty ("max-width");
-    }
-  
-    for (var i = 0; i < captionElements.length; i++) {
-      var element = captionElements[i];
-      element.style.removeProperty("max-width");
-    }
+  // Fade in elements with the "hideme" class on scroll
+  $(window).scroll(function() {
+    // Check the location of each desired element
+    $('.hideme').each(function(i) {
+      var top_of_object = $(this).offset().top;
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      
+      // If the object is completely visible in the window, fade it in
+      if (top_of_object < bottom_of_window){
+        $(this).animate({'opacity':'1'}, 1500);
+      }
+    });
   });
+
+  // Set up intersection observer for title animations on scroll
+  document.addEventListener("DOMContentLoaded", function() {
+    function handleIntersection(entries, observer) {
+      entries.forEach((entry, index) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animate-on-scroll');
+          observer.unobserve(entry.target);
+        }
+      });
+    }
+  
+    // Target the elements you want to animate on scroll
+    const titleContainers = document.querySelectorAll('.title .wobble, .title .slide-in');
+  
+    // Observe each target element for animation
+    titleContainers.forEach(container => {
+      const titleObserver = new IntersectionObserver(handleIntersection, { threshold: 0.5 });
+      titleObserver.observe(container);
+    });
+  });
+
+  // Timeline fade in on scroll
+  $('.item').fadeIn('slow');
+});
