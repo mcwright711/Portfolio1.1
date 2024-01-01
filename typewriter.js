382daabe897ef391fb195f45bb6024e47f91a882
ASOwnerYT <asowner@outlook.com>
2024-01-01 16:44:39 +1300

    Fix typewritter effect

--- typewriter.js
+++ typewriter.js
@@ -50,8 +50,4 @@ window.onload = function () {
       new TxtType(elements[i], JSON.parse(toRotate), period);
     }
   }
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".typewriter > .wrap { border-right: 0.08em solid #fff}";
-  document.body.appendChild(css);
 };
