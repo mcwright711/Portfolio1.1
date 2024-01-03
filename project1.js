4f3e049a361e35567ed9635c89f599d346260148
mcwright711 <118693689+mcwright711@users.noreply.github.com>
2024-01-02 20:56:23 -0700

    adding mouse animation
    adding mouse animation, updated jquery and bootstrap bundle in bottom of body

---
+++ project1.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  // Add active class to nav-item and nav-link elements when they have the "active" class
+  $(".nav-item.active, .nav-link.active").addClass("active");
+
+  // Remove the hover effect for active elements
+  $(
+    ".nav-item.active:not(.nav-item.dropdown.active), .nav-link.active:not(.nav-link.dropdown-toggle.active)"
+  ).hover(
+    function () {
+      $(this).addClass("no-hover");
+    },
+    function () {
+      $(this).removeClass("no-hover");
+    }
+  );
+  //Mousecursor effect on au post pg
+let $mouseCursor = $(".cursor");
+
+$(document).on('mousemove', function (e) {
+  $mouseCursor.css({
+    top: e.pageY,
+    left: e.pageX
+  });
+});
+navLinks.foreach(link=>)
+function(){
+  $(this).addclass (".link-grow")
+}
+  // Scrollspy effect
+  $("section").scrollspy({ target: "#navbar-example2" });
+});
